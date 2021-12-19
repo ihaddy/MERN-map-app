4077d7c1fcdb7d28acc36d521ba8dd60d9c21049
ih <ihaddy@gmail.com>
2021-12-19 12:48:26 -0500

    expand new place page and add usecallback hook to prevent infinite rerendering of functions

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, {useCallback} from "react";
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/util/validators";
 import "./NewPlace.css";
 
 export default function NewPlace() {
+    
+    const titleInputHandler = useCallback((id, value, isValid) = {
+
+    }, [])
+    const descriptionInputHandler = useCallback((id, value, isValid) = {
+
+    }, [])
+
+
   return (
     <form className="place-form">
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
         errorText="Please enter a valid title"
         validators={[VALIDATOR_REQUIRE()]}
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        type="text"
+        label="Description"
+        errorText="Please enter a valid Description (At least 10 characters)"
+        validators={[VALIDATOR_MINLENGTH(10)]}
+        onInput={descriptionInputHandler}
       />
     </form>
   );
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import { validate } from "../../util/validators";
 import "./Input.css";
 
@@ -27,6 +27,11 @@ export default function Input(props) {
     isValid: false,
     isTouched: false,
   });
+  
+  useEffect(() => {
+      props.onInput(props.id, inputState.value, inputState.isValid)
+  }, [props.id, props.value, props.isValid, props.onInput])
+
   const changeHandler = (e) => {
     dispatch({
       type: "CHANGE",
