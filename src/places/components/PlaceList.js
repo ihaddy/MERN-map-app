8b014434f89598883021d46c70a2be763258723b
ih <ihaddy@gmail.com>
2021-12-31 09:48:03 -0500

    add custom button component

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Card from "../../shared/components/UIElements/Card";
 import PlaceItem from "./PlaceItem";
+import Button from "../../shared/components/FormElements/Button";
 import "./PlaceList.css";
 export default function PlaceList(props) {
   if (props.items.length === 0) {
     return (
       <div className="place-list center">
         <Card>
-          <h2> No places found for this user</h2>
-          <button>Share Place</button>
+          <h2> No places found for this user. Create one?</h2>
+          <Button to="/places/new">Share Place</Button>
         </Card>
       </div>
     );
